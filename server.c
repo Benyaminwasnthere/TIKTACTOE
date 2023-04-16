937c5e116f95750c90cc6eec3702e82aed19c465
Benyaminwasnthere <124644188+Benyaminwasnthere@users.noreply.github.com>
2023-04-16 19:56:09 -0400

    Update server.c

--- server.c
+++ server.c
@@ -5,19 +5,40 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#define X 1
+#define O -1
 #define PORT 8000
 #define MAX_BUFFER 100
-void chat(int clientsock){
+struct Gamers{
+    int playero;
+    int playerx;
+}typedef Gamers;
+
+
+
+
+
+
+
+void *chat(void* p){
+    char board [3][3] ;
+    Gamers *gamers = p;
     char buff[MAX_BUFFER];
     int n;
-     for (;;) {
+    int temp_read, temp_write;
+    int current_player = 0;
+    bzero(&board[0], sizeof(board));
+    for (;;) {
+        temp_read = current_player == 0? gamers->playerx : gamers->playero;
+        temp_write = current_player == 0? gamers->playero : gamers->playerx;
+        current_player = current_player == 0? 1: 0;
         bzero(buff, MAX_BUFFER);
-        read(clientsock, buff, sizeof(buff));
+        read(temp_read, buff, sizeof(buff));
         printf("From client: %s\t ", buff);
         bzero(buff, MAX_BUFFER);
         n = 0;
         while ((buff[n++] = getchar()) != '\n');
-        write(clientsock, buff, sizeof(buff));    
+        write(temp_write, buff, sizeof(buff));    
 
 
         if (strncmp("exit", buff, 4) == 0) {
@@ -25,10 +46,13 @@ void chat(int clientsock){
             break;
         }
      }
+     return NULL;
+
 }
 void* server (void*p){
+    Gamers *gamers;
     int rv=0;
-     int sock,clientsock;
+     int sock,clientsock,clientsockx;
     struct sockaddr_in servaddr, clientinfo;
     printf("In server thread...\n");
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -57,9 +81,8 @@ void* server (void*p){
 
 
 
-
-
-
+// clientsock is made and is used to update clientsockx, later clientsock is updated again and is client sock0
+    int flag = 0;
 
     for(;;){
         if ((listen(sock, 4)) != 0) {
@@ -67,16 +90,31 @@ void* server (void*p){
             return NULL;
         }
         printf("Server listening..\n");
-        int size=sizeof(clientinfo);
-        clientsock=accept(sock,(struct sockaddr*)&clientinfo,&size);
+        socklen_t size=sizeof(clientinfo);
+            
+
+
+        clientsock=accept(sock,(struct sockaddr*)&clientinfo, &size);
         if(clientsock<0){
             printf("server accept failed...\n");
             return NULL;
         }
         printf("server accept the client...\n");
-        
-        chat(clientsock);
-        close(clientsock);
+        if(flag ==0){
+            clientsockx=clientsock;
+            flag=1;
+            printf("User X...\n");
+        }else{
+            pthread_t thr;
+            flag=0;
+            gamers= (Gamers*)malloc(sizeof(Gamers));
+            gamers->playero=clientsock;
+            gamers->playerx=clientsockx;
+            printf("User O...\n");
+            pthread_create(&thr,NULL,chat,(void*)gamers);
+        }
+        //chat(clientsock);
+        //close(clientsock);
     
     }
     pthread_exit(&rv);
