b9068ba9d716a3e85da68a7fb057e163e83dca32
Benyaminwasnthere <124644188+Benyaminwasnthere@users.noreply.github.com>
2023-04-17 00:47:48 -0400

    Update checkwinner.c

--- checkwinner.c
+++ checkwinner.c
@@ -5,7 +5,7 @@ void checkResult(char board[3][3]) {
     int flag = 1;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (board[i][j] == '\0') {
+            if (board[i][j] == '.') {
                 flag = 0;
                 break;
             }
